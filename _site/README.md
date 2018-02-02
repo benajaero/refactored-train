18ff4c0478d392b2a98674090d93ee69a4576f8e
Blaze349 <blaze349@protonmail.com>
2018-02-02 19:05:49 +1030

    commit yml

---
+++ _site/README.md
@@ -0,0 +1,3 @@
+# refactored-train
+
+This repo will serve as the journal for my personal project and whatever else is required.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Why I'm Using Arch As My Base",
+        "excerpt":"Upon careful consideration and research, I’ve decided to use Arch Linux as the base for my operating system.An important aspect in creating an optimal programming environment is the actual virtual virtual environment. The operating system manages processes, memory usage and input/output streams. It’s composed of various programs that interlock together...","categories": [],
+        "tags": [],
+        "url": "https://blaze349.github.io/refactored-train/journal/2018-01-27-Justification-For-Using-Arch/",
+        "teaser":null},{
+        "title": "Idea Station",
+        "excerpt":"Personal Project: Creating optimal coding environmentConsists of:  Hardware (custom computer tuned for my purposes)  Operating System (specialized linux distribution)  Scripting Language (creating a MIT LISP parser and interpreter)","categories": [],
+        "tags": [],
+        "url": "https://blaze349.github.io/refactored-train/2017/12/15/Idea-Station.html",
+        "teaser":null},{
+        "title": "Website Directory",
+        "excerpt":"PC BuildingCustom Linux Distro  http://www.tuxradar.com/content/how-build-your-own-linux-distroProgramming LanguageDesigning the language:  http://www.theenterprisearchitect.eu/blog/2013/02/14/designing-the-next-programming-language-understand-how-people-learn/  http://www.paulgraham.com/langdes.html  https://www.info.ucl.ac.be/~pvr/VanRoyChapter.pdf  https://en.wikibooks.org/wiki/Introduction_to_Programming_Languages/Grammars  https://en.wikipedia.org/wiki/Semantics_(computer_science)  https://en.wikipedia.org/wiki/Syntax_(programming_languages)Parsing  http://www.lihaoyi.com/post/EasyParsingwithParserCombinators.html  https://softwareengineering.stackexchange.com/questions/338665/when-to-use-a-parser-combinator-when-to-use-a-parser-generator  http://aquamentus.com/flex_bison.html  https://www.safaribooksonline.com/library/view/flex-bison/9780596805418/ch01.html  https://www.gnu.org/software/bison/manual/bison.html  https://www.cs.rochester.edu/~nelson/courses/csc_173/grammars/cfg.html  https://stackoverflow.com/questions/6713240/what-is-a-context-free-grammar","categories": [],
+        "tags": [],
+        "url": "https://blaze349.github.io/refactored-train/2017/12/15/Website-Directory.html",
+        "teaser":null},{
+        "title": "Pc Final Part  List",
+        "excerpt":"The final part list for the computer.PCPartPicker part list / Price breakdown by merchant Type Item Price CPU Intel - Core i7-8700 3.2GHz 6-Core Processor $485.00 @ Umart CPU Cooler Deepcool - GAMMAXX 400 74.3 CFM CPU Cooler $35.00 @ Umart Motherboard Asus - Prime Z370-P ATX LGA1151 Motherboard $219.00...","categories": [],
+        "tags": [],
+        "url": "https://blaze349.github.io/refactored-train/2018/01/03/PC-Final-Part-List.html",
+        "teaser":null}]
